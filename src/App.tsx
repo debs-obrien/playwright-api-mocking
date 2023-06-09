ea4186d09ff7218c9ecbdc6389ceda0e966bcbfc
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-06-09 20:27:52 +0200

    fix: api call to fruit and random num

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-function getRandomInt(max = 12) {
-  return Math.floor(Math.random() * max)
-}
-
 function FetchAPI() {
+  const num = Math.floor(Math.random() * 12)
   const [fruit, setFruit] = useState(null)
 
   useEffect(() => {
     fetch(
-      'https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json'
+      'https://raw.githubusercontent.com/debs-obrien/playwright-api-mocking/main/fruit.json'
     )
       .then((response) => response.json())
       .then((data) => setFruit(data))
@@ -25,8 +22,8 @@ function FetchAPI() {
       <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
         <div className="w-full md:w-1/3 bg-white grid place-items-center">
           <img
-            src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="tailwind logo"
+            src={fruit[num].img}
+            alt={fruit[num].name}
             className="rounded-xl"
           />
         </div>
@@ -68,17 +65,19 @@ function FetchAPI() {
             </div>
           </div>
           <h3 className="font-black text-gray-800 md:text-3xl text-xl">
-            {fruit[getRandomInt()].Name}
+            {fruit[num].name}
           </h3>
           <p className="md:text-lg text-gray-500 text-base">
-            The best kept secret of The Bahamas is the country’s sheer size and
-            diversity. With 16 major islands, The Bahamas is an unmatched
-            destination
+            {fruit[num].family}
           </p>
-          <p className="text-xl font-black text-gray-800">
+          <p className="md:text-lg text-gray-500 text-base">
+            {fruit[num].genus}
+          </p>
+
+          {/* <p className="text-xl font-black text-gray-800">
             $110
             <span className="font-normal text-gray-600 text-base">/night</span>
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
