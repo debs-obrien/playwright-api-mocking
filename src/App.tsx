5c30459876dceb119feb533d0444ed46578c6f5b
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-06-12 16:50:58 +0200

    chore: add stars to code

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ type Fruit = {
     protein: number
   }
   img: string
+  stars: number
 }
 
 function FetchAPI() {
@@ -32,6 +33,7 @@ function FetchAPI() {
           protein: 0,
         },
         img: '',
+        stars: 0,
       }
 
   useEffect(() => {
@@ -71,24 +73,10 @@ function FetchAPI() {
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
               </svg>
               <p className="text-gray-600 font-bold text-sm ml-1">
-                4.96
-                <span className="text-gray-500 font-normal">(76 reviews)</span>
+                {randomFruit.stars}
               </p>
             </div> */}
-            {/* <div className="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-pink-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </div> */}
+
             <div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
               {randomFruit.genus}
             </div>
@@ -104,11 +92,6 @@ function FetchAPI() {
             <li>Carbohydrates: {randomFruit.nutritions.carbohydrates}</li>
             <li>Protein: {randomFruit.nutritions.protein}</li>
           </ul>
-
-          {/* <p className="text-xl font-black text-gray-800">
-            $110
-            <span className="font-normal text-gray-600 text-base">/night</span>
-          </p> */}
         </div>
       </div>
     </div>
