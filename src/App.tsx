c077692531ad01bdbde4eea17e0dd647880ebaca
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2023-06-13 15:14:36 +0200

    feat: tests and modify ui

--- src/App.tsx
+++ src/App.tsx
@@ -17,31 +17,16 @@ type Fruit = {
 }
 
 function FetchAPI() {
-  const num: number = Math.floor(Math.random() * 19)
-  const [fruit, setFruit] = useState<Fruit[] | null>(null)
-  const randomFruit: Fruit = fruit
-    ? fruit[num]
-    : {
-        name: '',
-        family: '',
-        genus: '',
-        nutritions: {
-          calories: 0,
-          fat: 0,
-          sugar: 0,
-          carbohydrates: 0,
-          protein: 0,
-        },
-        img: '',
-        stars: 0,
-      }
+  const [fruit, setFruit] = useState<Fruit | null>(null)
 
   useEffect(() => {
     fetch(
       'https://raw.githubusercontent.com/debs-obrien/playwright-api-mocking/main/fruit.json'
     )
       .then((response) => response.json())
-      .then((data) => setFruit(data))
+      .then((fruits) => {
+        setFruit(fruits[Math.floor(Math.random() * fruits.length)])
+      })
   }, [])
 
   if (fruit === null) {
@@ -52,45 +37,44 @@ function FetchAPI() {
     <div className="flex flex-col justify-center">
       <div className="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
         <div className="w-full md:w-1/3 bg-white grid place-items-center">
-          <img
-            src={randomFruit.img}
-            alt={randomFruit.name}
-            className="rounded-xl"
-          />
+          <img src={fruit.img} alt={fruit.name} className="rounded-xl" />
         </div>
         <div className="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
           <div className="flex justify-between item-center">
             <p className="text-gray-500 font-medium hidden md:block">
-              {randomFruit.family}
+              {fruit.family}
             </p>
-            {/* <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              </svg>
-              <p className="text-gray-600 font-bold text-sm ml-1">
-                {randomFruit.stars}
-              </p>
-            </div> */}
+            {fruit.stars ? (
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-label="star"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <p className="text-gray-600 font-bold text-sm ml-1">
+                  {fruit.stars}
+                </p>
+              </div>
+            ) : null}
 
             <div className="bg-gray-200 px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
-              {randomFruit.genus}
+              {fruit.genus}
             </div>
           </div>
           <h3 className="font-black text-gray-800 md:text-3xl text-xl">
-            {randomFruit.name}
+            {fruit.name}
           </h3>
 
           <ul className="text-xl text-gray-800">
-            <li>Calories: {randomFruit.nutritions.calories}</li>
-            <li>Fat: {randomFruit.nutritions.fat}</li>
-            <li>Sugar: {randomFruit.nutritions.sugar}</li>
-            <li>Carbohydrates: {randomFruit.nutritions.carbohydrates}</li>
-            <li>Protein: {randomFruit.nutritions.protein}</li>
+            <li>Calories: {fruit.nutritions.calories}</li>
+            <li>Fat: {fruit.nutritions.fat}</li>
+            <li>Sugar: {fruit.nutritions.sugar}</li>
+            <li>Carbohydrates: {fruit.nutritions.carbohydrates}</li>
+            <li>Protein: {fruit.nutritions.protein}</li>
           </ul>
         </div>
       </div>
--- tests/api-mocking.spec.ts
+++ tests/api-mocking.spec.ts
@@ -1,34 +1,57 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('mock and modify', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173/')
-  })
-
   test("mocks a fruit and doesn't call api", async ({ page }) => {
+    // Mock the api call
     await page.route(
       'https://raw.githubusercontent.com/debs-obrien/playwright-api-mocking/main/fruit.json',
       async (route) => {
-        const json = {
-          name: 'Blueberry',
-        }
+        const json = [
+          {
+            name: 'Strawberry',
+            id: 3,
+            family: 'Rosaceae',
+            order: 'Rosales',
+            genus: 'Fragaria',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/7/73/La_Trinidad_strawberries.jpg?20070506152740',
+            nutritions: {
+              calories: 29,
+              fat: 0.4,
+              sugar: 5.4,
+              carbohydrates: 5.5,
+              protein: 0.8,
+            },
+          },
+        ]
         await route.fulfill({ json })
       }
     )
-    await expect(page.getByRole('heading', { name: 'Blueberry' })).toBeVisible()
+    // Go to the page
+    await page.goto('http://localhost:5173/')
+    // Assert that the fruit is visible
+    await expect(
+      page.getByRole('heading', { name: 'Strawberry' })
+    ).toBeVisible()
   })
 
-  test('gets a fruit from api and modifys it', async ({ page }) => {
+  test('gets the json from api and adds to it', async ({ page }) => {
+    // Get the response and add to it
     await page.route(
       'https://raw.githubusercontent.com/debs-obrien/playwright-api-mocking/main/fruit.json',
       async (route) => {
         const response = await route.fetch()
         const json = await response.json()
-        console.log(json)
-        json['name'] = 'Blueberry'
+        for (const fruit of json) {
+          fruit.stars = 5
+        }
+
         await route.fulfill({ response, json })
       }
     )
-    await expect(page.getByRole('heading', { name: 'Blueberry' })).toBeVisible()
+    // Go to the page
+    await page.goto('http://localhost:5173/')
+    // Assert that the stars are visible
+    await expect(page.getByRole('img', { name: 'star' })).toBeVisible()
+    await expect(page.getByText('5', { exact: true })).toBeVisible()
   })
 })
